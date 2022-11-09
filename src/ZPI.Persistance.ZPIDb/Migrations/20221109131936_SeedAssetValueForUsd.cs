440f9cc84bddbea48ae213d44d27ec1289c3a08d
barte525 <bartek5251@gmail.com>
2022-11-09 14:24:29 +0100

    add assetValue=1 dolar

---
+++ src/ZPI.Persistance.ZPIDb/Migrations/20221109131936_SeedAssetValueForUsd.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace ZPI.Persistance.ZPIDb.Migrations
+{
+    public partial class SeedAssetValueForUsd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "zpi",
+                table: "AssetValues",
+                columns: new[] { "Identifier", "AssetIdentifier", "TimeStamp", "Value" },
+                values: new object[] { -1L, "usd", new NodaTime.OffsetDateTime(new NodaTime.LocalDateTime(1, 1, 1, 0, 0), NodaTime.Offset.FromHours(0)), 1.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "zpi",
+                table: "AssetValues",
+                keyColumn: "Identifier",
+                keyValue: -1L);
+        }
+    }
+}
