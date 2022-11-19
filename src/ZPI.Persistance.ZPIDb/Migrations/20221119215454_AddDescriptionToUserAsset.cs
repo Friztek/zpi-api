bb29e1d07130cd90efc55aa17c9afa9573176c8c
barte525 <bartek5251@gmail.com>
2022-11-19 22:59:14 +0100

    add migration

---
+++ src/ZPI.Persistance.ZPIDb/Migrations/20221119215454_AddDescriptionToUserAsset.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZPI.Persistance.ZPIDb.Migrations
+{
+    public partial class AddDescriptionToUserAsset : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "zpi",
+                table: "UserAssets",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "zpi",
+                table: "UserAssets",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
